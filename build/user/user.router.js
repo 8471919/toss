17b59f70ccf3d27c298476e7113f3bac5e29ea5a
8471919 <7471919@naver.com>
2021-08-02 20:15:35 +0900

    add(First):Money.controller, Money.service

--- build/user/user.router.js
+++ build/user/user.router.js
@@ -7,6 +7,9 @@ const express_1 = __importDefault(require("express"));
 const user_controller_1 = __importDefault(require("./user.controller"));
 const userRouter = express_1.default.Router();
 // userRouter.get("/", userController.print);
+//userRouter.methods('/', parameter)에서
+//parameter의 this가 undefined이므로, bind를 사용하거나 class의 함수를 만들 경우
+//화살표함수를 사용한다.
 userRouter.get("/", user_controller_1.default.getAdmin);
 userRouter.post("/", user_controller_1.default.login);
 exports.default = userRouter;
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import userService, { UserService } from "./user.serivice";
+import userService, { UserService } from "./user.service";
 
 class UserController {
     private service: UserService;
@@ -8,18 +8,16 @@ class UserController {
         this.service = userService;
     }
 
+    //await this.service가 express에서 methods를 통해 호출될 경우에는 변수에 배열형태로 담겨서
+    //const handler = flatten(slice.call(arguments)) 형태로 들어가기 때문에
+    //화살표함수를 이용한다.
     getAdmin = async (req: Request, res: Response, next: NextFunction) => {
         const email = "adimn";
         const password = "admin";
 
         const [admin] = await this.service.getOne(email, password);
-        console.log(this);
         res.status(200).send(admin);
     };
-
-    async print() {
-        console.log(this);
-    }
 
     async login(req: Request, res: Response, next: NextFunction) {
         const { email, password } = req.body;
